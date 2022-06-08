5de17aae2a30a16fc23b85662be6a253114aa663
Gustavo Farias <gustavogarcia266@hotmail.com>
2022-06-08 14:51:15 -0300

    feat: add style buttons/ refactored code

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// Variables
 const btnSend = document.querySelector(".todo_button");
 const todoList = document.querySelector(".todo_list");
 const todoInput = document.querySelector(".todo_input");
 const todoSelect = document.querySelector("#option_main");
 
-btnSend.addEventListener("click", (event) => {
+// Events
+btnSend.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
   event.preventDefault();
 
   //Todo DIV
@@ -19,54 +24,63 @@ btnSend.addEventListener("click", (event) => {
 
   //CHECK MARK BUTTON
   const completeButton = document.createElement("button");
-  completeButton.innerHTML = "V";
+  const completeImg = document.createElement("img");
+  completeImg.src = './images/icon-complete.png';
+  completeButton.appendChild(completeImg);
   completeButton.classList.add("complete_btn");
   todoDiv.appendChild(completeButton);
 
   completeButton.addEventListener("click", () => {
-    todoDiv.classList.toggle("completed");
-    completeButton.style.display = "none";
-  });
+     completeTodoList(todoDiv, completeButton) 
+    });
+    
 
   //DELETE TODO
   const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "X";
+  const deleteImg = document.createElement("img");
+  deleteImg.src = './images/icon-delete.png';
+  deleteButton.appendChild(deleteImg);
   deleteButton.classList.add("delete_btn");
   todoDiv.appendChild(deleteButton);
 
-  deleteButton.addEventListener("click", () => {
-    todoDiv.remove(this);
-  });
+  deleteButton.addEventListener("click", () => todoDiv.remove(this));
 
+  // Add todo
   todoList.appendChild(todoDiv);
 
   //SELECT OPTION
-  todoSelect.addEventListener("click", () => {
-    if (todoSelect.value === "all") {
-      for (let obj = 0; obj < todoList.children.length; obj++) {
+  todoSelect.addEventListener("click", selectTodo);
+}
+
+const selectTodo = () => {
+  if (todoSelect.value === "all") {
+    for (let obj = 0; obj < todoList.children.length; obj++) {
+      todoList.children[obj].classList.remove("delete-todo");
+    }
+  }
+
+  if (todoSelect.value === "completed") {
+    for (let obj = 0; obj < todoList.children.length; obj++) {
+      if (todoList.children[obj].classList.contains("completed")) {
+        todoList.children[obj].classList.remove("delete-todo");
+      } else {
+        todoList.children[obj].classList.add("delete-todo");
+      }
+    }
+  }
+
+  if (todoSelect.value === "pendent") {
+    for (let obj = 0; obj < todoList.children.length; obj++) {
+      if (todoList.children[obj].classList.contains("completed")) {
+        todoList.children[obj].classList.add("delete-todo");
+      } else if (todoList.children[obj].classList.contains("delete-todo")) {
         todoList.children[obj].classList.remove("delete-todo");
       }
     }
+  }
+}
 
-    if (todoSelect.value === "completed") {
-      for (let obj = 0; obj < todoList.children.length; obj++) {
-        if (todoList.children[obj].classList.contains("completed")) {
-          todoList.children[obj].classList.remove("delete-todo");
-        }
-        else {
-          todoList.children[obj].classList.add("delete-todo");
-        }
-      }
-    }
-
-    if (todoSelect.value === "pendent") {
-      for (let obj = 0; obj < todoList.children.length; obj++) {
-        if (todoList.children[obj].classList.contains("completed")) {
-          todoList.children[obj].classList.add("delete-todo");
-        } else if (todoList.children[obj].classList.contains("delete-todo")) {
-          todoList.children[obj].classList.remove("delete-todo");
-        }
-      }
-    }
-  });
-});
+const completeTodoList = (todoDiv, completeButton) => {
+  todoDiv.classList.toggle("completed");
+  completeButton.style.display = "none";
+}
